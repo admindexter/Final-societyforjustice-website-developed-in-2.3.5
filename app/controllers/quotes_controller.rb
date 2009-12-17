73a70854bc59eec74e848f4cc073b9038920eb42
Dextersmart Harry <dextersmart@gmail.com>
2009-12-17 11:46:36 +0100

    Commit after press correction

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -14,18 +14,6 @@ class QuotesController < ApplicationController
     render :layout => 'application'
   end
 
-  # GET /quotes/1
-  # GET /quotes/1.xml
-  def show
-  	@last_five_presses = Press.find(:all, :limit => 5, :order => "created_at DESC")
-
-    @quote = Quote.find(params[:id])
-
-    @title = "#{@quote.passage.slice(0, 70)}" + " ..."
-
-    render :layout => 'application'
-  end
-
   # GET /quotes/1/edit
   def edit
     @title = "Editing daily quote"
@@ -33,49 +21,16 @@ class QuotesController < ApplicationController
     @quote = Quote.find(params[:id])
   end
 
-  # POST /quotes
-  # POST /quotes.xml
-  def create
-    @quote = Quote.new(params[:quote])
-
-    respond_to do |format|
-      if @quote.save
-        flash[:notice] = 'Quote was successfully created.'
-        format.html { redirect_to(@quote) }
-        format.xml  { render :xml => @quote, :status => :created, :location => @quote }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @quote.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
   # PUT /quotes/1
   # PUT /quotes/1.xml
   def update
     @quote = Quote.find(params[:id])
 
-    respond_to do |format|
-      if @quote.update_attributes(params[:quote])
-        flash[:notice] = 'Quote was successfully updated.'
-        format.html { redirect_to(@quote) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @quote.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /quotes/1
-  # DELETE /quotes/1.xml
-  def destroy
-    @quote = Quote.find(params[:id])
-    @quote.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(quotes_url) }
-      format.xml  { head :ok }
+    if @quote.update_attributes(params[:quote])
+      flash[:notice] = 'Quote was successfully updated.'
+        redirect_to home_path
+    else
+      render :action => "edit"
     end
   end
 end
