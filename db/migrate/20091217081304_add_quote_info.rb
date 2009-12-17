d7df84c88a3a4dcd3f373247af6b8be63446bb36
Dextersmart Harry <dextersmart@gmail.com>
2009-12-17 09:25:49 +0100

    Commit after add_quote_info

---
+++ db/migrate/20091217081304_add_quote_info.rb
@@ -0,0 +1,17 @@
+class AddQuoteInfo < ActiveRecord::Migration
+  def self.up
+    Quote.delete_all
+
+    Quote.create(
+    :passage => "Isaiah 58:6",
+    :content =>
+    %{<p>
+      To loose the bands of wickedness, to undo the heavy burdens,
+      and to let the oppressed go free, and that ye break every yoke?</p>})
+  end
+
+  def self.down
+    Quote.delete_all
+  end
+end
+
